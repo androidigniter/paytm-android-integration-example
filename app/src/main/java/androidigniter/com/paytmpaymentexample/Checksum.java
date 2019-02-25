e4c193dcff71acd5ca541e72d49277d2cb8a54c6
Androidigniter <aigniter2210@gmail.com>
2019-02-25 11:37:47 +0530

    Final Build
    Paytm Integration in Android Example Final Build

---
+++ app/src/main/java/androidigniter/com/paytmpaymentexample/Checksum.java
@@ -0,0 +1,37 @@
+package androidigniter.com.paytmpaymentexample;
+
+import com.google.gson.annotations.SerializedName;
+
+/**
+ * Created by AndroidIgniter on 25/02/2019.
+ */
+
+public class Checksum {
+
+    @SerializedName("CHECKSUMHASH")
+    private String checksumHash;
+
+    @SerializedName("ORDER_ID")
+    private String orderId;
+
+    @SerializedName("payt_STATUS")
+    private String paytStatus;
+
+    public Checksum(String checksumHash, String orderId, String paytStatus) {
+        this.checksumHash = checksumHash;
+        this.orderId = orderId;
+        this.paytStatus = paytStatus;
+    }
+
+    public String getChecksumHash() {
+        return checksumHash;
+    }
+
+    public String getOrderId() {
+        return orderId;
+    }
+
+    public String getPaytStatus() {
+        return paytStatus;
+    }
+}
